0f2d5aefb00e9ecc4be65a3dd0ee5571b3e3e0e2
Jeff Boenig <jboenig@nc.rr.com>
2018-09-01 11:29:15 -0400

    Cleanup and documentation

--- GameEngine/Conversation.cs
+++ GameEngine/Conversation.cs
@@ -10,6 +10,11 @@ namespace AdventureGameEngine
         void Hear(Conversation conversation, IConversationParticipant source, string dialogText);
     }
 
+    /// <summary>
+    /// This class encapsulates a conversation between multiple participants.
+    /// Any object that implements <see cref="IConversationParticipant"/> can
+    /// listen (hear) the conversation.
+    /// </summary>
     public sealed class Conversation
     {
         private List<IConversationParticipant> participants;
@@ -24,6 +29,16 @@ namespace AdventureGameEngine
             return new Conversation();
         }
 
+        /// <summary>
+        /// Starts a new conversation with one participant.
+        /// </summary>
+        /// <param name="participant1">
+        /// Participant in the conversation.
+        /// </param>
+        /// <returns>
+        /// Returns a new <see cref="Conversation"/> object containing
+        /// a single participant.
+        /// </returns>
         public static Conversation Start(IConversationParticipant participant1)
         {
             var conversation = new Conversation();
@@ -31,6 +46,15 @@ namespace AdventureGameEngine
             return conversation;
         }
 
+        /// <summary>
+        /// Starts a new conversation with two participants.
+        /// </summary>
+        /// <param name="participant1">First participant</param>
+        /// <param name="participant2">Second participant</param>
+        /// <returns>
+        /// Returns a new <see cref="Conversation"/> object containing
+        /// the two participants.
+        /// </returns>
         public static Conversation Start(IConversationParticipant participant1, IConversationParticipant participant2)
         {
             var conversation = new Conversation();
@@ -39,6 +63,15 @@ namespace AdventureGameEngine
             return conversation;
         }
 
+        /// <summary>
+        /// Determines if the specified name is a participant in
+        /// the conversation.
+        /// </summary>
+        /// <param name="participantName">Name of participant to check</param>
+        /// <returns>
+        /// Returns true if the conversation contains a participant with the
+        /// specified name, otherwise returns false.
+        /// </returns>
         public bool IsParticipant(string participantName)
         {
             if (string.IsNullOrEmpty(participantName))
@@ -52,6 +85,10 @@ namespace AdventureGameEngine
             return (existingParticipant != null);
         }
 
+        /// <summary>
+        /// Adds the specified participant to the conversation.
+        /// </summary>
+        /// <param name="participant">Participant to add</param>
         public void Join(IConversationParticipant participant)
         {
             if (participant == null)
@@ -71,6 +108,12 @@ namespace AdventureGameEngine
             this.participants.Add(participant);
         }
 
+        /// <summary>
+        /// Removes the specified participant from the conversation.
+        /// </summary>
+        /// <param name="participantName">
+        /// Name of participant to remove.
+        /// </param>
         public void Leave(string participantName)
         {
             var existingParticipant = (from p in this.participants
@@ -83,15 +126,23 @@ namespace AdventureGameEngine
             }
         }
 
-        public void Say(string sourceName, string dialogText)
+        /// <summary>
+        /// Broadcasts the specified message from the specified participant
+        /// to all other participants in the conversation.
+        /// </summary>
+        /// <param name="participantName">
+        /// Name of participant speaking.
+        /// </param>
+        /// <param name="dialogText">Message to send</param>
+        public void Say(string participantName, string dialogText)
         {
             var source = (from p in this.participants
-                          where p.Name == sourceName
+                          where p.Name == participantName
                           select p).FirstOrDefault();
 
             if (source == null)
             {
-                var msg = string.Format("{0} is not a participant in this conversation", sourceName);
+                var msg = string.Format("{0} is not a participant in this conversation", participantName);
                 throw new ArgumentException(msg, "participantName");
             }
 
